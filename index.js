bc2968a6dec49a8d7e9d67d0e41ba8734a2609b0
Jordan Poles <jpoles1@gmail.com>
2016-01-17 00:58:39 -0600

    Working fully, it seems!

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+var mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'))
 //Load Env Data
 var env = require('node-env-file');
 try{
@@ -34,19 +39,64 @@ var Sightings = mongoose.model('sightings', sightingSchema);
 var db = mongoose.connection;
 app.get("/tracker", function(req, res){
   var today = new Date();
+  console.log(today)
   console.log("new log:", req.query.bristol)
-  var turd = new Sightings({bristol: req.query.bristol, date: new Date(req.query.year, req.query.month, req.query.day), hour: req.query.hour, dayofweek: weekday[req.query.dow]})
+  var turd = new Sightings({bristol: req.query.bristol, date: today, hour: req.query.hour, dayofweek: weekday[req.query.dow]})
   turd.save(function(err) {
     if (err) throw err;
     console.log('User saved successfully!');
   });
   res.send('Logged!');
 });
-app.get('/public', function(req, res) {
-  app.use(express.static('public/home.html'));
+app.get("/fetchTimeseries", function(req, res){
+  Sightings.find(function (err, data) {
+    if (err) return console.error(err);
+    res.send(JSON.stringify(data));
+  })
 });
+app.get("/fetchdow", function(req, res){
+  Sightings.find(function (err, data) {
+    if (err) return console.error(err);
+    //res.send(JSON.stringify(data));
+  })
+  var agg = [
+    {$group: {
+      _id: "$dayofweek",
+      count: {$sum: 1}
+    }}
+  ];
+  Sightings.aggregate(agg, function(err, data){
+    if (err) { return def.reject(err); }
+    plotdata = [];
+    for(i in data){
+      plotdata.push({name: data[i]._id, y: data[i].count});
+    }
+    res.send(JSON.stringify(plotdata));
+  });
+});
+app.get("/fetchTimeofday", function(req, res){
+  Sightings.find(function (err, data) {
+    if (err) return console.error(err);
+    //res.send(JSON.stringify(data));
+  })
+  var agg = [
+    {$group: {
+      _id: "$hour",
+      count: {$sum: 1}
+    }}
+  ];
+  Sightings.aggregate(agg, function(err, data){
+    if (err) { return def.reject(err); }
+    plotdata = [];
+    for(i in data){
+      plotdata.push({name: data[i]._id, y: data[i].count});
+    }
+    res.send(JSON.stringify(plotdata));
+  });
+});
+app.use('/public', express.static('public'));
 app.get("/", function(req, res){
-  res.send('Welcome to the Poop Logger!');
+  res.render('viz', { title: 'Hey', message: 'Hello there!'});
 });
 var port = process.env.PORT || 8000;
 app.listen(port, function () {
